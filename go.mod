75724cd678aba2ae714ff64edf20ffab143e5ece
zachklingbeil <zach@zachklingbeil.com>
2025-12-13 13:30:44 -0800

    keyboard overlay is coming along.

--- go.mod
+++ go.mod
@@ -2,12 +2,9 @@ module github.com/timefactoryio/demo
 
 go 1.25.5
 
-require github.com/timefactoryio/frame v0.2.38
+require github.com/timefactoryio/frame v0.2.43
 
 require (
-	github.com/felixge/httpsnoop v1.0.4 // indirect
-	github.com/gorilla/handlers v1.5.2 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/litao91/goldmark-mathjax v0.0.0-20210217064022-a43cf739a50f // indirect
 	github.com/yuin/goldmark v1.7.13 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/timefactoryio/frame"
 	"github.com/timefactoryio/frame/zero"
@@ -14,41 +12,19 @@ func main() {
 	api := os.Getenv("API_URL")
 
 	f := frame.NewFrame(pathless, api)
+	Keyboard(f)
 	f.Home("the perpetual motion machine", "timefactoryio", "")
 	f.README(f.ToBytes("./README.md"))
 	f.BuildSlides("slides")
 	f.AddPath("./img")
 	// BuildVideo(f, "./video/beef.mp4")
 	// BuildVideo(f, "./video/hungary.mp4")
-	f.Keyboard(true)
 	f.Serve()
 	select {}
 }
 
-func BuildVideo(f *frame.Frame, filePath string) *zero.One {
-	f.AddFile(filePath, "video")
-	name := filepath.Base(filePath)
-	name = name[:len(name)-len(filepath.Ext(name))]
-
-	video := f.Video("")
-	css := f.CSS(f.VideoCSS())
-	js := f.JS(fmt.Sprintf(`
-(async function() {
-    const el = pathless.frame().querySelector('video');
-    if (!el) return;
-
-    const { data: src } = await pathless.fetch(apiUrl + '/video/%s', { key: 'video-%s' });
-    el.src = src;
-
-    el.addEventListener('loadedmetadata', () => {
-        el.currentTime = pathless.state().t || 0;
-    }, { once: true });
-
-    pathless.keybind(
-        k => { if (k === ' ') el.paused ? el.play() : el.pause(); },
-        () => { pathless.update('t', el.currentTime || 0); el.pause(); }
-    );
-})();
-    `, name, name))
-	return f.Build("video", true, video, &css, &js)
+func Keyboard(f *frame.Frame) *zero.One {
+	template := f.HTML(f.ToString("./keyboard.html"))
+	final := f.Build("keyboard", true, template)
+	return final
 }
