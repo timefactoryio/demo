71978883d387ef155a30bf0f1fc20fdbf5566ce8
zachklingbeil <zach@zachklingbeil.com>
2025-12-28 11:12:30 -0800

    commit

--- main.go
+++ main.go
@@ -10,11 +10,11 @@ func main() {
 	pathless := os.Getenv("PATHLESS_URL")
 	api := os.Getenv("API_URL")
 
-	f := frame.NewFrame(pathless, api)
-	f.AddPath("./img")
+	f := frame.NewFrame(pathless, api, "./keyboard.html")
+	f.Reader("./img")
 	f.Home("the perpetual motion machine", "timefactoryio", "")
-	f.Text(f.ToBytes("./README.md"))
+	f.Text("./README.md")
 	f.Slides("slides")
-	f.Serve()
+	f.Start()
 	select {}
 }
