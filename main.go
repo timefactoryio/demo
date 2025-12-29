837a2e95a6714c17cd1ad56559c9fecc3679133f
zachklingbeil <zach@zachklingbeil.com>
2025-12-29 13:19:26 -0800

    simplify

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	pathless := os.Getenv("PATHLESS_URL")
 	api := os.Getenv("API_URL")
 
-	f := frame.NewFrame(pathless, api, "./keyboard.html")
+	f := frame.NewFrame(pathless, api)
 	f.Reader("./img")
 	f.Home("the perpetual motion machine", "timefactoryio", "")
 	f.Text("./README.md")
