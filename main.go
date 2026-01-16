3fb3cfa8850985762ca0cdb2cce0aaad99f0cda0
zachklingbeil <zach@zachklingbeil.com>
2026-01-16 11:56:49 -0800

    v1.0.0

--- main.go
+++ main.go
@@ -4,15 +4,6 @@ import (
 	"github.com/timefactoryio/frame"
 )
 
-// func main() {
-// 	f := frame.NewFrame()
-// 	f.Home(os.Getenv("LOGO_PATH"), os.Getenv("HEADING"))
-// 	f.Text(os.Getenv("TEXT_URL"))
-// 	f.Slides(os.Getenv("SLIDES_PATH"))
-// 	f.Start(os.Getenv("PATHLESS"))
-// 	select {}
-// }
-
 func main() {
 	f := frame.NewFrame()
 	f.Home("/timefactory.svg", "the perpetual motion machine")
